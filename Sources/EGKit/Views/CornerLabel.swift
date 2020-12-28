4f18447aafba94e9d86f23d9d5f9f843589cc92f
dev-02 <>
2020-12-28 11:16:31 +0800

    CornerLabel

---
+++ Sources/EGKit/Views/CornerLabel.swift
@@ -0,0 +1,24 @@
+//
+//  CornerLabel.swift
+//  
+//
+//  Created by dev-02 on 2020/12/28.
+//
+
+import UIKit
+
+@IBDesignable
+class CornerLabel: UILabel {
+
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+
+    @IBInspectable var masksToBounds: Bool = false {
+        didSet {
+            layer.masksToBounds = masksToBounds
+        }
+    }
+}
