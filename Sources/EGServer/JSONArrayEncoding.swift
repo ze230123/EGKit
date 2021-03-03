752f48a01292b027eba00c6a4f84c5da5f7a15fd
dev-02 <>
2021-03-03 10:20:46 +0800

    JSONArrayEncoding

---
+++ Sources/EGServer/JSONArrayEncoding.swift
@@ -0,0 +1,32 @@
+//
+//  JSONArrayEncoding.swift
+//  
+//
+//  Created by dev-02 on 2021/3/3.
+//
+
+import Alamofire
+import Foundation
+
+struct JSONArrayEncoding: ParameterEncoding {
+    static let `default` = JSONArrayEncoding()
+
+    func encode(_ urlRequest: URLRequestConvertible, with parameters: Parameters?) throws -> URLRequest {
+        var request = try urlRequest.asURLRequest()
+
+        guard let json = parameters?["jsonArray"] else {
+            return request
+        }
+
+        let data = try JSONSerialization.data(withJSONObject: json, options: [])
+
+        if request.value(forHTTPHeaderField: "Content-Type") == nil {
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        }
+
+        request.httpBody = data
+
+        return request
+    }
+}
+
