1e23064a8c8cbd590c9ec16c7a4064568bbebea6
dev-02 <>
2021-05-13 14:02:11 +0800

    cacheresult访问控制

--- Sources/EGServer/CacheResult.swift
+++ Sources/EGServer/CacheResult.swift
@@ -10,11 +10,11 @@ import Foundation
 /// 缓存用到的模型
 public struct CacheResult<Element> {
     /// 服务器`Result`jsonString
-    let jsonString: String?
+    public let jsonString: String?
     /// `Result`解析后的模型
-    let result: Element
+    public let result: Element
 
-    init(jsonString: String?, result: Element) {
+    public init(jsonString: String?, result: Element) {
         self.jsonString = jsonString
         self.result = result
     }
